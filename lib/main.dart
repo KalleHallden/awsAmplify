875105b31ecb444f534591e5718ab827a506583e
kallehallden <halldenkalle@gmail.com>
2024-12-12 09:50:59 +0100

    aws login with phone

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:aws_amplify/ui_elements/signup_form.dart';
 import 'package:flutter/material.dart';
 import 'amplify_outputs.dart';
 
@@ -29,6 +30,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Authenticator(
+	    //initialStep: AuthenticatorStep.signUp,
+	    //  signUpForm: SignUpForm.custom(
+	//	      fields: [
+	//		      SignUpFormField.phoneNumber(required: true)
+	//	      ], 
+
+	//	      ),
       child: MaterialApp(
         builder: Authenticator.builder(),
         home: const Scaffold(
